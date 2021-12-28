4fe9ce80dec384cb3f2664265a40486af6efaef1
rodentskie <rodentskie@gmail.com>
2021-12-28 09:52:45 +0800

    feat: add socket

--- src/index.js
+++ src/index.js
@@ -6,17 +6,20 @@ const dotenv = require('dotenv');
 dotenv.config();
 const app = new Koa();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser());
 
 const PORT = process.env.PORT || '3000';
 
-// routes
-
 const server = app.listen(PORT, () =>
-    console.log(`Server is listening on port http://localhost:${PORT}.`),
+  console.log(`Server is listening on port http://localhost:${PORT}.`),
 );
+const io = require('socket.io')(server);
 
 app.use(async (ctx) => (ctx.body = { msg: `Welcome to this API.` }));
 
-module.exports = { server }
+io.on('connection', (socket) => {
+  console.log('A socket has been connected.', socket.handshake);
+});
+
+module.exports = { app, io };
